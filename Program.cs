c9d9e54e0fff8e0d767c30cbf119cefecdbe9031
Дмитрий Сухарев <dimka231001@mail.ru>
2020-06-06 07:19:57 +0300

    Добавлен подсчет код-ва запросов

--- Program.cs
+++ Program.cs
@@ -16,9 +16,43 @@ namespace ConsoleApp1
 {
     class Program
     {
+        struct struct_General_Provision
+        {
+            public int number_result;
+            public string name_search;
+            public struct_General_Provision(int number_result, string name_search)
+            {
+                this.number_result = number_result;
+                this.name_search = name_search;
+            }
+
+        }
+        struct struct_Duties
+        {
+            public int number_result;
+            public string name_search;
+            public struct_Duties(int number_result, string name_search)
+            {
+                this.number_result = number_result;
+                this.name_search = name_search;
+            }
+
+        }
+        struct struct_Rights
+        {
+            public int number_result;
+            public string name_search;
+            public struct_Rights(int number_result, string name_search)
+            {
+                this.number_result = number_result;
+                this.name_search = name_search;
+            }
+
+        }
         [STAThread]
         static void Main(string[] args)
         {
+            
             List<string>general_Provisions = new List<string> { "Общие положения", "ОБЩИЕ ПОЛОЖЕНИЯ"};
             List<string> duties = new List<string> { "Обязанности","обязанности","ОБЯЗАННОСТИ"};
             List<string> rights = new List<string> { "Права", "права", "ПРАВА" };
@@ -29,6 +63,7 @@ namespace ConsoleApp1
             int id_duties = 0,id_rights = 0, id_general_Provisions = 0;
            
             List<string> parText = new List<string> { };
+            //Открытие файла и выгрузка его в список
             OpenFileDialog dialog = new OpenFileDialog
             {
                 Filter = "Файлы MS Word |*.rtf",
@@ -52,6 +87,7 @@ namespace ConsoleApp1
                 app.Quit();
                
             }
+            //Нахождение строк основных разделителей на группы
             for (int i = 0; i < parText.Count; i++)
             {
                 foreach (string list in duties)
@@ -67,16 +103,20 @@ namespace ConsoleApp1
                     if (parText[i].Contains(list)) id_general_Provisions = i;
                 }
             }
+
+            //Загрузка общих положений
             while (parText[id_general_Provisions+2].ToString().Length > 5)
             {
                 general_Provisions_list.Add(parText[id_general_Provisions + 2]);
                 id_general_Provisions++;
             }
+            //Звгрузка обязанностей
             while (parText[id_duties + 2].ToString().Length > 5)
             {
                 duties_list.Add(parText[id_duties + 2]);
                 id_duties++;
             }
+            //Загрузка прав
             while (parText[id_rights + 2].ToString().Length > 5)
             {
                 rights_list.Add(parText[id_rights + 2]);
@@ -84,10 +124,36 @@ namespace ConsoleApp1
                 
             }
 
+            List<struct_General_Provision> s_G_P = new List<struct_General_Provision>();
+            List<struct_Duties> s_D = new List<struct_Duties>();
+            List<struct_Rights> s_R = new List<struct_Rights>();
+            foreach (string list in general_Provisions)
+            {
+                Random rnd = new Random();
+                int value = rnd.Next(800,1400);
+                s_G_P.Add(new struct_General_Provision(pars(list),list));
+                Thread.Sleep(value);
+            }
+            foreach (string list in duties_list)
+            {
+                Random rnd = new Random();
+                int value = rnd.Next(800, 1400);
+                s_D.Add(new struct_Duties(pars(list), list));
+                Thread.Sleep(value);
+            }
+            foreach (string list in rights_list)
+            {
+                Random rnd = new Random();
+                int value = rnd.Next(800, 1400);
+                s_R.Add(new struct_Rights(pars(list),list));
+                Thread.Sleep(value);
+            }
+
+            
             Console.ReadKey();
 
         }
-
+        //Парс колличества результатов
         static int pars(string name)
         {
             string url = "https://google.com/search?q=";
